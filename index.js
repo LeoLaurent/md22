a12c0b970c49e57d15bd0b4f9ca2eb93a937be2d
LeoLaurent <laurentj.leo@gmail.com>
2021-09-22 14:50:25 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+const express = require('express')
+const app = express()
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.use(function (req, res, next) {
+    date = new Date(Date.now())
+    console.log('Time:', date.toLocaleDateString(), date.toLocaleTimeString(), "; url :", req.url);
+    next(); // sans cette ligne on ne pourra pas poursuivre.
+})
+
+app.use("/static", express.static(path.join(__dirname, '/static')))
+
+app.get('/', (req, res) => {
+    res.redirect(301, '/static/index.html')
+})
+
+
+app.use(function (req, res) {
+    console.log("Jamais 4 sans 4 : " + req.url);
+
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+
+    res.end("<html><head><title>Quatre roues motrices</title></head><body><h1>Fils</h1><img  src=\"https://www.leblogauto.com/wp-content/uploads/2020/04/Peugeot-404-1.jpg\" /></body></html>");
+
+})
+
+app.listen(port, hostname);
+console.log(`Server running at http://${hostname}:${port}/`);
